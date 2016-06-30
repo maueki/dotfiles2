e17c2058359acb9e5a75b2c334a11ca57e4f5230
Ueki Masaru <masaru.ueki@itage.co.jp>
2016-06-30 13:20:30 +0900

    emacs: 不要パッケージ削除

--- .emacs.d/init/other.el
+++ .emacs.d/init/other.el
@@ -4,12 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(require 'guess-offset)
-
 (require 'elixir-mode)
 (require 'qml-mode)
-
-(require 'elixir-mix)
-(global-elixir-mix-mode)
 
 (require 'adoc-mode)
