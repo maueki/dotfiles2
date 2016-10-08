93958553854f2d3052c8bacce98370f3787b00e1
Masaru Ueki <masaru.ueki@itage.co.jp>
2016-10-08 14:35:33 +0900

    emacs: elixir yassnipet改善

--- .emacs.d/init/yasnippet_init.el
+++ .emacs.d/init/yasnippet_init.el
@@ -3,3 +3,7 @@
 (yas-global-mode 1)
 (add-to-list 'yas/root-directory "~/.emacs.d/snippets")
 (yas/initialize)
+
+(add-hook 'elixir-mode-hook
+          (lambda ()
+            (setq yas-buffer-local-condition 't)))
