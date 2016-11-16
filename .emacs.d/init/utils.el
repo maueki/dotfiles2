3264daf6f3a00f2e3e7c57588eebb360cf4fabcb
Ueki Masaru <masaru.ueki@itage.co.jp>
2016-11-16 13:16:21 +0900

    emacs: tmux-new-windowをdiredでも動作するように

--- .emacs.d/init/utils.el
+++ .emacs.d/init/utils.el
@@ -6,6 +6,9 @@
 ; 現在開いているバッファのディレクトリをtmuxで開く
 (defun tmux-new-window ()
   (interactive)
-  (let ((fname buffer-file-name))
-    (if (stringp fname)
-        (shell-command (concat "tmux new-window -c \"$(dirname " fname ")\"")))))
+  (if (string-equal major-mode "dired-mode")
+      (let ((dirname (expand-file-name default-directory)))
+        (shell-command (concat "tmux new-window -c \"" dirname "\"")))
+    (let ((fname buffer-file-name))
+      (if (stringp fname)
+          (shell-command (concat "tmux new-window -c \"$(dirname " fname ")\""))))))
