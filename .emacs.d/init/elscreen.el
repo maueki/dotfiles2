e2cb8b8970caecaecd1bb32ce0fe8a812ee995ab
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-02-14 15:49:50 +0900

    emacs: Disable (kdd "M-'") and (kdb "M-;") for avoiding to conflict other package's key binding.

--- .emacs.d/init/elscreen.el
+++ .emacs.d/init/elscreen.el
@@ -7,8 +7,8 @@
 
 ; C-', C-;でscreenを移動
 ;(global-set-key [?\C-'] 'elscreen-next)
-(define-key global-map (kbd "M-'") 'elscreen-next)
+;(define-key global-map (kbd "M-'") 'elscreen-next)
 ;(global-set-key [?\C-;] 'elscreen-previous)
-(define-key global-map (kbd "M-;") 'elscreen-previous)
+;(define-key global-map (kbd "M-;") 'elscreen-previous)
 ;(global-set-key [?\C-t] 'elscreen-clone)
 (define-key global-map (kbd "M-t") 'elscreen-clone)
