8f4ee81e2341b9b86605fcb2270089c03da79147
Ueki Masaru <masaru.ueki@itage.co.jp>
2019-03-04 13:38:18 +0900

    emacs: eglot for C/C++

--- .emacs.d/init/prog.el
+++ .emacs.d/init/prog.el
@@ -14,3 +14,11 @@
 (use-package flymake
   :config
   (setq flymake-no-changes-timeout 2))
+
+(use-package eglot
+  :bind (:map eglot-mode-map
+              ("C-c C-d" . eglot-help-at-point)
+              ("C-c C-r" . eglot-code-actions))
+  :hook
+  ((c-mode-common . eglot-ensure))
+  )
--- .emacs.d/init/rust.el
+++ .emacs.d/init/rust.el
@@ -1,10 +1,4 @@
 
-(use-package eglot
-  :bind (:map eglot-mode-map
-              ("C-c C-d" . eglot-help-at-point)
-              ("C-c C-r" . eglot-code-actions))
-  )
-
 (use-package rustic
   :config
   (setq rustic-rls-pkg 'eglot)
