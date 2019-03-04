f4bd39dc5780c2bfcb6bec41a744a16839b2c8a0
Ueki Masaru <masaru.ueki@itage.co.jp>
2019-03-04 15:00:22 +0900

    emacs: Use projectile

--- .emacs.d/init/prog.el
+++ .emacs.d/init/prog.el
@@ -15,6 +15,15 @@
   :config
   (setq flymake-no-changes-timeout 2))
 
+(use-package projectile
+  :config
+  (defun projectile-project-find-function (dir)
+    (let* ((root (projectile-project-root dir)))
+      (and root (cons 'transient root))))
+  (with-eval-after-load 'project
+    (add-to-list 'project-find-functions 'projectile-project-find-function))
+  )
+
 (use-package eglot
   :bind (:map eglot-mode-map
               ("C-c C-d" . eglot-help-at-point)
