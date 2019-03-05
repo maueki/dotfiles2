16dfd7c781647703590779ee68c7e3064067a361
Masaru Ueki <masaru.ueki@itage.co.jp>
2019-03-05 22:27:10 +0900

    emacs: Improve prog.el

--- .emacs.d/init/prog.el
+++ .emacs.d/init/prog.el
@@ -1,10 +1,21 @@
 
 (use-package company
-  :bind (:map company-mode-map
-              ("C-c TAB" . company-complete))
+  :bind
+  (:map company-mode-map
+        ("C-c TAB" . company-complete))
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("TAB" . company-complete-selection))
+  (:map company-search-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous))
   :config
   (global-company-mode)
-
+  :custom
+  (company-idle-delay 0)
+  (company-minimum-prefix-length .5)
+  (company-selection-wrap-around t)
   :diminish company-mode)
 
 (use-package irony
@@ -14,8 +25,8 @@
     (push 'company-irony company-backends)))
 
 (use-package flymake
-  :config
-  (setq flymake-no-changes-timeout 2))
+  :custom
+  (flymake-no-changes-timeout 2))
 
 (use-package projectile
   :config
