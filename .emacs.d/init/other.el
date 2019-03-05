224f6d3b54f5a4aa3111e0968df91cd3fc89c95d
Ueki Masaru <masaru.ueki@itage.co.jp>
2019-03-05 17:45:51 +0900

    emacs: Add many packages

--- .emacs.d/init/other.el
+++ .emacs.d/init/other.el
@@ -61,3 +61,56 @@
   :init
   (editorconfig-mode 1)
 )
+
+(use-package doom-themes
+  :if window-system
+  :custom
+  (doom-themes-enable-italic t)
+  (doom-themes-enable-bold t)
+  :custom-face
+;  (doom-modeline-bar ((t (:background "#6272a4"))))
+  :config
+  (load-theme 'doom-one t)
+;  (doom-themes-neotree-config)
+  (doom-themes-org-config)
+  )
+
+(use-package doom-modeline
+  :if window-system
+  :custom
+  (doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (doom-modeline-icon t)
+  :hook
+  (after-init . doom-modeline-mode)
+  :config
+;  (doom-modeline-def-modeline 'main
+;    '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
+;    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker))
+  )
+
+(use-package which-key
+  :diminish which-key-mode
+  :hook (after-init . which-key-mode))
+
+(use-package amx)
+
+(use-package all-the-icons)
+
+(use-package git-gutter
+  :custom
+  (git-gutter:modified-sign "~")
+  (git-gutter:added-sign    "+")
+  (git-gutter:deleted-sign  "-")
+  :custom-face
+  (git-gutter:modified ((t (:background "#f1fa8c"))))
+  (git-gutter:added    ((t (:background "#50fa7b"))))
+  (git-gutter:deleted  ((t (:background "#ff79c6"))))
+  :config
+  (global-git-gutter-mode +1))
+
+(use-package ace-window
+  :bind
+  (("C-c C-j" . ace-window))
+  :custom-face
+  (aw-leading-char-face ((t (:height 3.0 :foreground "#f1fa8c"))))
+  )
