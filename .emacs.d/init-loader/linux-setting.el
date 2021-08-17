6b317a088a041eef1cf3f47941b3049bd7fd72fe
Masaru Ueki <masaru.ueki@itage.co.jp>
2021-08-17 17:44:27 +0900

    Use ansi-color for compilation mode

--- .emacs.d/init-loader/linux-setting.el
+++ .emacs.d/init-loader/linux-setting.el
@@ -1,1 +1,8 @@
 (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans Mono CJK JP"))
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
