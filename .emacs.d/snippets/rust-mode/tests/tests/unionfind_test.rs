f40f54e6600d24db6b80bc5250fc2e8e758c5b6e
Masaru Ueki <masaru.ueki@itage.co.jp>
2023-10-28 16:31:19 +0900

    atcoder: use ac_library::Dsu

--- .emacs.d/snippets/rust-mode/tests/tests/unionfind_test.rs
+++ .emacs.d/snippets/rust-mode/tests/tests/unionfind_test.rs
@@ -3,9 +3,9 @@ include!("../../uf");
 
 #[test]
 fn uf_test() {
-    let mut uf = UnionFind::new(3);
+    let mut uf = Dsu::new(3);
 
-    uf.unite(1,2);
+    uf.merge(1,2);
 
     assert_eq!(uf.size(1), 2);
     assert_eq!(uf.size(0), 1);
